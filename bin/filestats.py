93f2d94ddc05e24641e0051692d873cc05a43f33
Marcin Steć <marcin_stec@pl.ibm.com>
2024-07-16 18:44:47 +0200

    Sortowanie, rożne jednostki i CSV

--- bin/filestats.py
+++ bin/filestats.py
@@ -87,7 +87,7 @@ def dir_file_stats(depth, path, pattern):
 
     return measured_max_depth, myFileCount, myDirCount, myFailedDirs, mySize
 
-def bytes2human(size):
+def size2human(size):
     """
     Zwraca rozmiar podany w bajtach jako string human readable".
     """
@@ -98,19 +98,44 @@ def bytes2human(size):
         my_size = round(size / 1073741824, 3)
         ret = str(my_size) + ' GiB'
     elif size > 1048576:           # Megabajty
-        my_size = round(size / 1073741824, 3)
+        my_size = round(size / 1048576, 3)
         ret = str(my_size) + ' MiB'
     elif size > 1024:             # Kilobajty
         my_size = round(size / 1024, 3)
         ret = str(my_size) + ' KiB'
-    else:                           # bajty
+    else:                         # bajty
         ret = str(size) + ' B'
 
     return ret
 
-def print_file_stats(stats):
+def size2KIB(size):
     """
-    Drukuje ładnie podsumowanie zliczonych rozszerzeń plików
+    Bajty na kilobajty z dopiskiem +KiB
+    """
+    return str(round(size / 1024, 3)) + ' KiB'
+
+def size2MIB(size):
+    """
+    Bajty na  MiB z dopiskiem +MiB
+    """
+    return str(round(size / 1048576), 3) + ' MiB'
+
+def size2GIB(size):
+    """
+    Bajty na  GiB z dopiskiem +GiB
+    """
+    return str(round(size / 1073741824), 3) + ' GiB'
+
+def size2TIB(size):
+    """
+    Bajty na  TiB z dopiskiem +TiB
+    """
+    return str(round(size / 1099511627776), 3) + ' TiB'
+
+
+def print_stats_fancy(stats, units):
+    """
+    Drukuje ładnie z listy zliczonych rozszerzeń plików
     """
     cols, rows = os.get_terminal_size()
     line = "_" * cols
@@ -119,39 +144,94 @@ def print_file_stats(stats):
     cnt_pad = 20
     size_pad = 30
 
-    for key in stats:
-        human_size = bytes2human(stats[key][1])
-        ext_count = str(stats[key][0])
-        print(f'{ key.rjust(ext_pad) }:{ ext_count.rjust(cnt_pad) }{ human_size.rjust(size_pad) }')
+    for item in stats:  
+        if units == 'K':
+            print_units = size2KIB(item[2])
+        elif units == 'M':
+            print_units = size2MIB(item[2])
+        elif units == 'G':
+            print_units = size2GIB(item[2])
+        elif units == 'T':
+            print_units = size2TIB(item[2])
+        elif units == 'H':
+            print_units = size2human(item[2])
+        else:
+            print_units = str(item[2]) + ' B'
 
-def print_file_stats_csv(stats):
-    """
-    Drukuje ładnie podsumowanie zliczonych rozszerzeń plików
-    """
-    cols, rows = os.get_terminal_size()
-    line = "_" * cols
-    # Padding
-    ext_pad = 20
-    cnt_pad = 20
-    size_pad = 30
+        ext_count = str(item[1])
 
-    for key in stats:
-        human_size = bytes2human(stats[key][1])
-        ext_count = str(stats[key][0])
-        print(f'{ key },{ ext_count },{ human_size }')
+        print(f'{ item[0].rjust(ext_pad) }:{ ext_count.rjust(cnt_pad) }{ print_units.rjust(size_pad) }')
+
+def print_stats_csv(stats):
+    """
+    Drukuje csv z listy zliczonych rozszerzeń plików
+    """
+
+    for item in stats:
+        ext_count = str(item[1])
+        print(f'{ item[0] },{ item[1] },{ item[2] }')
+
+def by_ext(entry):
+    """
+    Funkcja pomocnicza do sortowania listy statystyk po rozszerzeniu. Zwraca entry[0]
+    """
+    print(f'by_size: {entry[0]}')
+    return entry[0]
+
+def by_count(entry):
+    """
+    Funkcja pomocnicza do sortowania listy statystyk po liczniku rozszerzeń. Zwraca entry[1]
+    """
+    return entry[1]
+
+def by_size(entry):
+    """
+    Funkcja pomocnicza do sortowania listy statystyk po rozmiarze. Zwraca entry[2]
+    """
+    return entry[2]
+
+def sorted_stats(stats, order):
+    """
+    Zwraca posortowaną według klucza order listę list skondtruowaną z hasha stats
+    """
+    stats_list = []
+    for k, v in stats.items():
+        stats_list.append([k] + v)
+
+    if order == 'asc_ext':
+        stats_list.sort(key=by_ext)
+    elif order == 'desc_ext':
+        stats_list.sort(key=by_ext, reverse=True)
+    elif order == 'asc_size':
+        stats_list.sort(key=by_size)
+    elif order == 'desc_size':
+        stats_list.sort(key=by_size, reverse=True)
+    elif order == 'asc_count':
+        stats_list.sort(key=by_count)
+    elif order == 'desc_count':
+        stats_list.sort(key=by_count, reverse=True)
+    return stats_list
 
 # Main
 start = timer()
 parser = argparse.ArgumentParser(description="Zliczanie plików po rozszerzeniach.")
 parser.add_argument('-i', '--ignorecase', help='Ignoruj wielkość liter w nazwie pliku. (Domyślnie: nie)', action='store_true')
 parser.add_argument('dir', nargs='?', default='.', help="Katalog do przeliczenia. (Domyslnie: bieżący)")
-parser_grp = parser.add_mutually_exclusive_group()
-parser_grp.add_argument('-u', '--upper', help="Konwertuj wszystkie rozszerzenia na WIELKIE litery", action="store_true")
-parser_grp.add_argument('-l', '--lower', help="Konwertuj wszystkie rozszerzenia na małe litery", action="store_true")
+
+conv_grp = parser.add_mutually_exclusive_group()
+conv_grp.add_argument('-u', '--upper', help="Konwertuj wszystkie rozszerzenia na WIELKIE litery", action="store_true")
+conv_grp.add_argument('-l', '--lower', help="Konwertuj wszystkie rozszerzenia na małe litery", action="store_true")
+
+
+parser.add_argument('-U', '--units', help="Konwersja na jednoswki: B - bajty, K - KIB, M - MIB, G - GiB, T - TiB, H - human redable.", choices=['B', 'K', 'M', 'G', 'T', 'H'], default='H')
+
 parser.add_argument('-c', '--comma', help='Output w formacie CSV', action='store_true')
+parser.add_argument('-s', '--sort', choices=['asc_ext', 'asc_count', 'asc_size', 'desc_ext', 'desc_count', 'desc_size'], default='desc_size', help='Sortowanie asc|desc po rozszerzeniu (ext), liczbie (count), rozmiarze (size)' )
+
 args = parser.parse_args()
 
 directory = args.dir
+print_units = args.units
 
 # Kompilowanie patternu tutaj, bo potem będzie szybciej. 
 if args.ignorecase:
@@ -167,22 +247,21 @@ elif args.lower:
 else:
     conv = 0
 
-print(f'Konwersja: { conv }')
+depth, FilesCount, DirsCount, FailedDirs, TotalBytes = dir_file_stats(0, directory, ext_pattern)
 
-if os.path.isdir(directory):       # parametr to katalog do zbadania
-    depth, FilesCount, DirsCount, FailedDirs, TotalBytes = dir_file_stats(0, directory, ext_pattern)
+stats_as_list = sorted_stats(FileStats, args.sort)
+if args.comma:
+    print_stats_csv(stats_as_list)
+else:
     print("\n")
-    if args.comma:
-        print_file_stats_csv(FileStats)
-    else:
-        print_file_stats(FileStats)
+    print_stats_fancy(stats_as_list, print_units)
     print("\n")
     print(f'Pliki i symlinki:'.rjust(20) + f'{ str(FilesCount).rjust(20, " ") }')
     print(f'Katalogi:'.rjust(20) + f'{ str(DirsCount).rjust(20, " ") }')
     print(f'Brak dost.:'.rjust(20) + f'{ str(FailedDirs).rjust(20, " ") }')
+    print(f'Liczba rozszerzeń:'.rjust(20) + f'{ str(len(FileStats)).rjust(20) }')
     print(f'\n\nMaksymana osiągnięta głębokość rekursji: { depth }.')
-    end = timer()
-    print(f'Czas wykonania { round(end - start, 2) }s.')
-else:
-    print(f'Prametr { directory } nie wygląda na katalog ¯\\_(ツ)_/¯')
-    exit(1)
+
+end = timer()
+print(f'Czas wykonania { round(end - start, 2) }s.')
+
